7088d6a23a2f9975b33c1714dde27f3202feefdb
이승주 <139853497+Leeseung-joo@users.noreply.github.com>
2025-09-25 22:45:21 +0900

    [level 2] Title: 가장 큰 수, Time: 400.84 ms, Memory: 141 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] numbers) {
+        
+        String[] arr = new String[numbers.length];
+        for(int i = 0; i < arr.length; i++){ //숫자를 문자열 배열로 변환
+            arr[i] = String.valueOf(numbers[i]);
+        }
+        //내림차순
+        Arrays.sort(arr, (o1,o2) -> (o2+o1).compareTo(o1+o2));
+        
+        
+        //이어 붙이기
+        StringBuilder sb = new StringBuilder();
+        for(String s : arr){
+            sb.append(s);
+        }
+        
+        if(sb.charAt(0) == '0'){
+            return "0";
+        }
+        
+        return sb.toString();
+    
+        
+    }
+}
