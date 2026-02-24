21ac664899c15da0ed88a5d3935e0f74f824f3b3
이승주 <139853497+Leeseung-joo@users.noreply.github.com>
2026-02-24 21:17:54 +0900

    [level 2] Title: 큰 수 만들기, Time: 72.62 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,18 +1,24 @@
 import java.util.*;
 class Solution {
     public String solution(String number, int k) {
-        StringBuilder sb = new StringBuilder();
-        int remove = k;
-
-        for (char c : number.toCharArray()) {
-            while (remove > 0 && sb.length() > 0 
-                   && sb.charAt(sb.length() - 1) < c) {
-                sb.deleteCharAt(sb.length() - 1);
-                remove--;
+       
+        Stack<Character> stack = new Stack<>();
+        int keep = number.length() - k; //남겨야하는 길이
+        
+        for(int i = 0; i < number.length(); i++){
+            char digit = number.charAt(i);
+            
+            while(!stack.isEmpty() && k > 0 && stack.peek() < digit){
+                stack.pop();
+                k--;
             }
-            sb.append(c);
+            stack.push(digit);
         }
-
-        return sb.substring(0, sb.length() - remove);
+    
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()){
+            sb.append(stack.pop());
+        }
+       return sb.reverse().substring(0,keep);
     }
 }
