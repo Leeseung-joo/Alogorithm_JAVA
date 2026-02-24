0733fe6bb81e074f55a44fdc598d1e3a70adda9e
이승주 <139853497+Leeseung-joo@users.noreply.github.com>
2026-02-24 22:15:31 +0900

    [level 2] Title: 타겟 넘버, Time: 5.99 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    static int cnt = 0;
+    public int solution(int[] numbers, int target) {
+        
+        
+        dfs(numbers, target, 0, 0);
+        return cnt;
+    }
+    
+    static void dfs(int[] numbers, int target, int idx, int sum){
+        
+        
+        if(idx == numbers.length){
+            if(target == sum){
+                cnt++;
+            }
+            return;
+        }
+        
+        dfs(numbers,target,idx+1, sum + numbers[idx]);
+        
+        dfs(numbers,target,idx+1, sum - numbers[idx]);
+    }
+}
