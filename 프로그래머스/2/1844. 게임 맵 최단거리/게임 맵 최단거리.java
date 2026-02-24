6838a5e838a059038cdb9ca998b05c203465d0d7
이승주 <139853497+Leeseung-joo@users.noreply.github.com>
2026-02-24 12:33:43 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.08 ms, Memory: 54.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,51 +1,66 @@
 import java.util.*;
-
 class Solution {
-    public int solution(int[][] maps) {
-        int n = maps.length;
-        int m = maps[0].length;
-
-        int[] dx = {0, 1, 0, -1};   // 오른, 아래, 왼, 위
-        int[] dy = {1, 0, -1, 0};
-
-        boolean[][] visited = new boolean[n][m];
-        ArrayDeque<Point> q = new ArrayDeque<>();
-
-        q.offer(new Point(0, 0, 1));
-        visited[0][0] = true;
-
-        while (!q.isEmpty()) {
-            Point p = q.poll();
-
-            // 도착했으면 거리 리턴
-            if (p.x == n - 1 && p.y == m - 1) {
-                return p.distance;
-            }
-
-            for (int i = 0; i < 4; i++) {
-                int nx = p.x + dx[i];
-                int ny = p.y + dy[i];
-
-                // 범위 밖
-                if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-                // 벽이거나 이미 방문
-                if (maps[nx][ny] == 0 || visited[nx][ny]) continue;
-
-                visited[nx][ny] = true;
-                q.offer(new Point(nx, ny, p.distance + 1));
-            }
-        }
-
-        // 도착 못 하면 -1
-        return -1;
-    }
-
-    static class Point {
-        int x, y, distance;
-        Point(int x, int y, int distance) {
+            
+    static int[][] maps;
+    static int[] dx = {1,0,-1,0};
+    static int[] dy = {0,1,0,-1};
+    static boolean[][] visited;
+    static int n;
+    static int m;
+    
+    static class Point{
+        int x,y;
+        int distance;
+        
+        Point(int x, int y, int distance){
             this.x = x;
             this.y = y;
             this.distance = distance;
         }
+
+        
+    }
+    public int solution(int[][] maps) {
+        this.n = maps.length;
+        this.m = maps[0].length;
+        this.maps = maps;
+        
+        visited = new boolean[n][m];
+        
+        
+        int distance = bfs(0,0,1);
+        return distance;
+        
+    }
+    
+    static int bfs(int x, int y, int distance){
+        ArrayDeque<Point> q = new ArrayDeque<>();
+        visited[x][y] = true;
+        q.offer(new Point(x,y,distance));
+        
+        while(!q.isEmpty()){
+            Point p = q.poll();
+            
+            if(p.x == n-1 && p.y == m-1) return p.distance;
+            
+            for(int i = 0; i < 4; i++){
+                int nx = p.x + dx[i];
+                int ny = p.y + dy[i];
+                
+                if(!isRange(nx,ny)) continue;
+               
+                if(maps[nx][ny] == 1 && !visited[nx][ny]){
+                    q.offer(new Point(nx,ny, p.distance+1));
+                    visited[nx][ny] = true;
+                }
+                
+            }
+        }
+        return -1;
+        
+    }
+    
+    static boolean isRange(int x,int y){
+        return x >= 0 && x < n && y >= 0 && y < m;
     }
 }
