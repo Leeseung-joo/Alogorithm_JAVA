2ea06994bbdad13583910b6b8303c3bcd39f4a31
이승주 <139853497+Leeseung-joo@users.noreply.github.com>
2026-02-24 19:19:25 +0900

    [level 2] Title: 구명보트, Time: 14.55 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,22 +1,27 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] people, int limit) {
-        Arrays.sort(people); // 몸무게 오름차순 정렬
-        int left = 0;                 // 가장 가벼운 사람
-        int right = people.length - 1; // 가장 무거운 사람
-        int boats = 0;                // 필요한 보트 수
-
-        while (left <= right) {
-            // 가장 가벼운 사람 + 가장 무거운 사람 같이 탈 수 있으면
-            if (people[left] + people[right] <= limit) {
-                left++; // 가벼운 사람도 탑승 완료
+        
+        
+        Arrays.sort(people);
+        
+        int left = 0;
+        int right = people.length-1;
+        int cnt = 0;
+        while(left <= right){
+            int temp = people[left] + people[right];
+            
+            if(temp > limit){
+                right--;
+                cnt++;
+            }else{
+                left++;
+                right--;
+                cnt++;
             }
-            // 무거운 사람은 항상 태움
-            right--;
-            boats++;
         }
-
-        return boats;
+        return cnt;
+        
+        
     }
 }
