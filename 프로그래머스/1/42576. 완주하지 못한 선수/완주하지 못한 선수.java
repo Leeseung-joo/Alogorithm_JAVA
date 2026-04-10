6b3af110caf3811d172d8b6920801b33ed5bf949
이승주 <139853497+Leeseung-joo@users.noreply.github.com>
2026-04-10 20:49:08 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 81.36 ms, Memory: 97.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(String s: participant){
+            map.put(s, map.getOrDefault(s,0)+ 1);
+        }
+        
+        for(String s : completion){
+            map.put(s,map.get(s)-1);
+        }
+        String result = "";
+        for(String s : map.keySet()){
+            if(map.get(s) > 0){
+                return s;
+            }
+        
+        }
+        return "";
+    }
+}
