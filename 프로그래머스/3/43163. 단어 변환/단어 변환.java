5a34fcbb6732d23e8082ef68903996bc1acebbd0
이승주 <139853497+Leeseung-joo@users.noreply.github.com>
2026-04-18 17:27:08 +0900

    [level 3] Title: 단어 변환, Time: 0.83 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,36 +1,44 @@
+import java.util.*;
 class Solution {
-    static boolean[] visited;
-    static int answer = 0;
-    public int solution(String begin, String target, String[] words) {
+    static class Node{
+        String word;
+        int cnt;
         
-        visited = new boolean[words.length];
-        
-        dfs(begin, target, words, 0);
-        return answer;
-    }
-    
-    public static void dfs(String begin, String target, String[] words, int cnt){
-        if(begin.equals(target)){
-            answer = cnt;
-            return;
+        Node(String word, int cnt){
+            this.word = word;
+            this.cnt = cnt;
         }
+    }
+    public int solution(String begin, String target, String[] words) {
+        int answer = 0;
         
-        for(int i = 0; i < words.length; i++){
-            if(visited[i]) continue;
+        ArrayDeque<Node> q = new ArrayDeque<>();
+        boolean[] visited = new boolean[words.length];
+        
+        q.offer(new Node(begin,0));
+        
+        while(!q.isEmpty()){
+            Node current = q.poll();
             
-            int k = 0; //같은 스펠링 몇개인지 세기
+            if(current.word.equals(target)) return current.cnt;
             
-            for(int j = 0; j < begin.length(); j++){
-                if(begin.charAt(j) == words[i].charAt(j)){
-                    k++;
+            for(int i = 0; i < words.length; i++){
+                if(!visited[i] && canChange(current.word, words[i])){
+                    visited[i] = true;
+                    q.offer(new Node(words[i], current.cnt+1));
                 }
             }
-            
-            if(k == begin.length() - 1){
-                visited[i] = true;
-                dfs(words[i], target,words,cnt+1);
-                visited[i] = false;
-            }
         }
+        
+        return 0;
+    }
+    
+    static boolean canChange(String current, String target){
+        int diff = 0;
+        
+        for(int i = 0; i < target.length(); i++){
+            if(current.charAt(i) != target.charAt(i)) diff++;
+        }
+        return diff == 1 ? true : false;
     }
 }
