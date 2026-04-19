cc86a9bc6fa3391dcca25f92e4a2901b8dc4d9e7
이승주 <139853497+Leeseung-joo@users.noreply.github.com>
2026-04-19 18:30:05 +0900

    [level 2] Title: 리코쳇 로봇, Time: 5.22 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -1,93 +1,97 @@
 import java.util.*;
 class Solution {
-    static int[] dx = new int[]{1,0,-1,0};
-    static int[] dy = new int[]{0,1,0,-1};
-    static String[][] map;
-    static int result = Integer.MAX_VALUE;
+    static char[][] map;
+    static boolean[][] visited;
+    static int[] dx = {1,0,-1,0};
+    static int[] dy = {0,1,0,-1};
     static int N;
     static int M;
-        
+    static int result = -1;
     
-    static class Point{
+    static class Node{
         int x;
         int y;
-        int cnt;
-        Point(int x, int y, int cnt){
-            this.x= x;
+        int cost;
+        
+        Node(int x,int y, int cost){
+            this.x = x;
             this.y = y;
-            this.cnt = cnt;
+            this.cost = cost;
         }
     }
     public int solution(String[] board) {
+        int answer = 0;
+        
+        map = new char[board.length][board[0].length()];
+        visited = new boolean[board.length][board[0].length()];
         
         N = board.length;
-        M = board[0].length();
+        M = board[0].length(); 
         
-        map = new String[N][M];
+        for(int i = 0; i < board.length; i++){
+            String line = board[i];
+            for(int j = 0; j < line.length(); j++){
+                map[i][j] = line.charAt(j);
+            }
+        }
         
-        
-        int startX = 0;
-        int startY = 0;
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < M; j++){
-                map[i][j] = String.valueOf(board[i].charAt(j));
-                if(map[i][j].equals("R")) {
-                    startX = i;
-                    startY = j;
+        for(int i = 0; i < map.length; i++){
+            for(int j = 0; j < map[0].length; j++){
+                if(map[i][j] == 'R'){
+                    bfs(i,j);
                 }
             }
         }
-        int answer = bfs(startX,startY);
+        return result;
         
-        return answer;
         
     }
     
-    
-    static int bfs(int x, int y){
-        int cnt = 0;
-        
-        ArrayDeque<Point> q = new ArrayDeque<>();
-        boolean[][] visited = new boolean[map.length][map[0].length];
+    static void bfs(int x,int y){
         visited[x][y] = true;
-        q.offer(new Point(x,y,0));
+        
+        ArrayDeque<Node> q = new ArrayDeque<>();
+        q.offer(new Node(x,y,0));
         
         while(!q.isEmpty()){
-            Point p = q.poll();
-            
-            if(map[p.x][p.y].equals("G")){
-                return p.cnt;
-            }
+            Node cur = q.poll();
             
             for(int i = 0; i < 4; i++){
-                int nx = p.x;
-                int ny = p.y;
+                int nx = cur.x;
+                int ny = cur.y;
                 
-                while(true){
-                    int tx = nx + dx[i];
-                     int ty = ny + dy[i];
-                if(!isRange(tx,ty) || map[tx][ty].equals("D")){
-                    break;
+               while(true){
+                   int fx = nx + dx[i];
+                   int fy = ny + dy[i];
+                   
+                   if(!inRange(fx,fy) || map[fx][fy] == 'D') break;
+                   
+                   nx = fx;
+                   ny = fy;
+                   
+               }
+                if(visited[nx][ny]) continue;
+                
+                if(map[nx][ny] == 'G'){
+                    result = cur.cost + 1;
+                    return;
                 }
-
-                    nx = tx;
-                    ny = ty;
-                    
-                }
-                if(!visited[nx][ny]){
-                q.offer(new Point(nx,ny,p.cnt+1));
+                
                 visited[nx][ny] = true;
+                q.offer(new Node(nx,ny, cur.cost + 1));
                 
-                }
+                
+                
+                
+                
             }
-        }
-    
-        return -1;
-        
-        
+}
+        return;
     }
     
-    static boolean isRange(int x, int y){
-        return x >= 0 && x < N && y >=0 && y < M;
+    static boolean inRange(int x,int y){
+        return x >= 0 && x < N && y >= 0 && y < M;
     }
+    
+    
 }
