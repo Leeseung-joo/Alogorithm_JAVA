2f68a26b2c6e3d1c76bb75ce73ebb00b032af26e
이승주 <139853497+Leeseung-joo@users.noreply.github.com>
2026-04-24 17:43:21 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260424/빙빙 돌며 숫자 사각형 채우기/snail-number-square.java
+++ 260424/빙빙 돌며 숫자 사각형 채우기/snail-number-square.java
@@ -2,28 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int n, m;
+    static int[][] arr;
+
+    // 오른쪽, 아래, 왼쪽, 위
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+
+    static boolean inRange(int x, int y) {
+        return x >= 0 && x < n && y >= 0 && y < m;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int[][] arr = new int[n][n];
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        // 오른쪽, 아래, 왼쪽, 위
-        int[] dx = {0, 1, 0, -1};
-        int[] dy = {1, 0, -1, 0};
+        arr = new int[n][m];
 
         int x = 0;
         int y = 0;
         int dir = 0;
 
-        for (int num = 1; num <= n * n; num++) {
+        for (int num = 1; num <= n * m; num++) {
             arr[x][y] = num;
 
             int nx = x + dx[dir];
             int ny = y + dy[dir];
 
-            // 범위를 벗어나거나 이미 숫자가 있으면 방향 전환
-            if (nx < 0 || nx >= n || ny < 0 || ny >= n || arr[nx][ny] != 0) {
+            if (!inRange(nx, ny) || arr[nx][ny] != 0) {
                 dir = (dir + 1) % 4;
                 nx = x + dx[dir];
                 ny = y + dy[dir];
@@ -36,7 +45,7 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < m; j++) {
                 sb.append(arr[i][j]).append(" ");
             }
             sb.append("\n");
