0254b165630960b7c4f5aed8ddfba92efe7daddd
이승주 <139853497+Leeseung-joo@users.noreply.github.com>
2026-04-27 22:51:12 +0900

    Title: 최고의 13위치 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260427/최고의 13위치 2/best-place-of-13-2.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int[][] arr;
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        int n = Integer.parseInt(br.readLine());
+
+        arr = new int[n][n];
+
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < n; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        int maxCnt = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n-2; j++){
+                for(int k = i+1; k < n; k++){
+                    for(int l = 0; l < n-2; l++){
+                        maxCnt = Math.max(maxCnt,arr[i][j] + arr[i][j+1] + arr[i][j+2] +arr[k][l] + arr[k][l+1]+ arr[k][l+2]);
+                    }
+                }
+            }
+        }
+        System.out.println(maxCnt);
+
+
+    }
+}
