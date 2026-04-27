e810de049c5f03035c44a191ae1227b0427f4101
이승주 <139853497+Leeseung-joo@users.noreply.github.com>
2026-04-27 22:57:05 +0900

    Title: 최고의 13위치 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260427/최고의 13위치 2/best-place-of-13-2.java
+++ 260427/최고의 13위치 2/best-place-of-13-2.java
@@ -21,8 +21,9 @@ public class Main {
         int maxCnt = 0;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n-2; j++){
-                for(int k = i+1; k < n; k++){
+                for(int k = i; k < n; k++){
                     for(int l = 0; l < n-2; l++){
+                        if(i == k && Math.abs(j - l) < 3) continue;
                         maxCnt = Math.max(maxCnt,arr[i][j] + arr[i][j+1] + arr[i][j+2] +arr[k][l] + arr[k][l+1]+ arr[k][l+2]);
                     }
                 }
